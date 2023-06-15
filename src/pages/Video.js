0f92e0f285c5206841ceaa9743ffceba442616e3
Alessandra Morita <contactalessandramorita@gmail.com>
2023-06-15 15:54:16 +0200

    Add video layout

---
+++ src/pages/Video.js
@@ -0,0 +1,32 @@
+import React, { useRef, useState } from "react";
+import "./Video.css";
+
+function Video() {
+  const videoRef = useRef(null);
+  const [play, setPlay] = useState(false);
+
+  function handleStart() {
+    if (!play) {
+      videoRef.current.play();
+      setPlay(true);
+    } else {
+      videoRef.current.pause();
+      setPlay(false);
+    }
+  };
+
+  return (
+    <div className="video-container">
+      <video
+        src="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z"
+        ref={videoRef}
+        onClick={handleStart}
+        loop
+      >
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  );
+}
+
+export default Video;
